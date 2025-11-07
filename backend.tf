b32d3a23beb6269b812dfaf5e259ccabd680d425
Egwu Vincent Oko <egwuoko76@gmail.com>
2025-11-08 00:08:34 +0100

    Another pipeline Update

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "berry-state-rg"
-    storage_account_name = "berrytfstate28791"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "berrytfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "terraform.berrytfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-    access_key           = "var.storage_account_key"
-  }
-}
